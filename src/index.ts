9eb9cda3583cebb7b2ab0b4e86a0dd14b5fa023b
eggacheb <115781643+eggacheb@users.noreply.github.com>
2024-08-16 16:06:27 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-"use strict";
-
 import environment from "@/lib/environment.ts";
 import config from "@/lib/config.ts";
 import "@/lib/initialize.ts";
 import server from "@/lib/server.ts";
 import routes from "@/api/routes/index.ts";
 import logger from "@/lib/logger.ts";
+import tokenManager from "@/lib/token-manager.ts";
+import sessionManager from "@/lib/session-manager.ts";
 
 const startupTime = performance.now();
 
@@ -17,6 +17,14 @@ const startupTime = performance.now();
   logger.info("Process id:", process.pid);
   logger.info("Environment:", environment.env);
   logger.info("Service name:", config.service.name);
+
+  // 初始化TokenManager并立即执行一次刷新
+  await tokenManager.refreshTokens();
+
+  // 设置定期清理session的任务
+  setInterval(() => {
+    sessionManager.cleanupSessions();
+  }, 5 * 60 * 1000); // 每5分钟清理一次
 
   server.attachRoutes(routes);
   await server.listen();
